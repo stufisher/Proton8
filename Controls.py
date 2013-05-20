72c44d19f2efce7882122242c30cc7d10a170e35
Stu Fisher <vxn01537@diamrl5371.home>
2013-05-20 21:14:49 +0100

    add kwargs and callback to FileBrowser

--- Controls.py
+++ Controls.py
@@ -5,11 +5,20 @@ import os
 # File browser control
 class FileBrowser:
 
-    def __init__(self, parent, mask, title, dir=False):
+    def __init__(self, parent, mask, title, *args, **kwargs):
+        self._callback = None
+        self._dir = False
+        
+        if 'dir' in kwargs:
+            self._dir = True
+        
+        if 'callback' in kwargs:
+            self._callback = kwargs['callback']
+    
+    
         self._parent = parent
         self._mask = mask
         self._title = title
-        self._dir = dir
         
         self._browse = wx.Button(parent, -1, 'Browse')#, size=(50,25))
         self._browse.Bind(wx.EVT_BUTTON, self._get_file)
@@ -27,12 +36,15 @@ class FileBrowser:
         
         if dlg.ShowModal() == wx.ID_OK:
             self._file.SetLabel(dlg.GetPath())
+            if self._callback is not None:
+                self._callback()
+            
 
         dlg.Destroy()
 
 
     def file(self):
-        return self._file.GetValue()
+        return str(self._file.GetValue())
     
     def sizer(self):
         return self._browse_sizer
